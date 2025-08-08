7d8c59c539df1ab2dcb77722ef31c64144fde9f6
Adamaq01 <adamthibert01@gmail.com>
2025-08-08 02:05:20 +0200

    Use CMake to compile

---
+++ generated/build_details.h
@@ -0,0 +1,8 @@
+#define BUILD_TIME 10:55:11
+#define BUILD_DATE Aug  3 2025
+#define BUILD_CHAR ANSI
+#define BUILD_CPU  X86
+#define BUILD_COMP Visual C++ 13.4
+#ifndef _MSC_VER
+ #define _MSC_VER  1944
+#endif
